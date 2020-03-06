8767015d79324a4547adbf6d0388c7d4b662da2c
Morris <m_morris104853@ats-hr420-dm25.fc.ca>
2020-03-06 10:00:51 -0500

    /

--- js/main.js
+++ js/main.js
@@ -4,19 +4,35 @@ let sigilButtons = document.querySelectorAll('.sigilContainer')
   lightBox = document.querySelector(".lightbox")
   closeLightbox = lightBox.querySelector('.close')
   gotVideo = lightBox.querySelector('video')
+
+
 function showLightbox() {
   //pop open lightbox and show content
   //start with a video
   //debugger
+  // need to get the class name from the element so we can match the video source
+  let targetName = this.className.split(" ")[1]; //this wil strip out the house name 
+  let targetSource = targetName.charAt(0).toUpperCase() + targetName.slice(1);
+  let newVideoSource = `video/house-${targetSource}.mp4`
+
+
+
   lightBox.classList.add('show-lightbox');
+  gotVideo.src = newVideoSource;
+  gotVideo.load();
   gotVideo.play();
+
 }
+
+
 
 function hideLightbox(){
   lightBox.classList.remove('show-lightbox');
   gotVideo.currentTime = 0;
   gotVideo.pause()
 }
+
+
 sigilButtons.forEach(button => button.addEventListener("click", showLightbox));
 closeLightbox.addEventListener("click", hideLightbox)
 })();
